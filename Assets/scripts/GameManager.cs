224c671791b58f0103d77ffad11800cfaf03ce18
Akshat <akshatyadav_250215@aitpune.edu.in>
2026-04-17 23:02:33 +0530

    Add Exponential Spotlight Growth and Fix Spotlight Shrinking Bug

--- Assets/scripts/GameManager.cs
+++ Assets/scripts/GameManager.cs
@@ -6,12 +6,21 @@ public class GameManager : MonoBehaviour
     // This makes it easy for other scripts to find the GameManager
     public static GameManager Instance; 
 
+    [SerializeField] private float inc_sl = 0.2f;
+
     public int currentScrap = 0;
     public TextMeshProUGUI scrapText; // The UI text on screen
     
     // Upgrade Variables
     public Transform spotlightMask; // The thing that gets bigger
-    public int upgradeCost = 10;
+    public int upgradeCost = 5;
+
+    // Upgrade limit tracking
+    private int upgradeCount = 0;
+    private const int MaxUpgrades = 10;
+
+    // Scale factor for exponential cost growth (e.g. 1.6 = 60% more expensive each time)
+    [SerializeField] private float costScaleFactor = 1.6f;
 
     void Awake()
     {
@@ -34,17 +43,37 @@ public class GameManager : MonoBehaviour
     // The Button will call this function
     public void BuyVisionUpgrade()
     {
+        Debug.Log($"BuyVisionUpgrade called! Scrap: {currentScrap}, Cost: {upgradeCost}, Upgrades: {upgradeCount}/{MaxUpgrades}");
+
+        // Already maxed out
+        if (upgradeCount >= MaxUpgrades)
+        {
+            Debug.Log("Vision fully upgraded!");
+            return;
+        }
+
         if (currentScrap >= upgradeCost)
         {
-            currentScrap -= upgradeCost; // Pay the cost
-            UpdateUI(); // Update the text
+            currentScrap -= upgradeCost; 
+            upgradeCount++;
 
-            // Make the spotlight 50% bigger!
-            spotlightMask.localScale += new Vector3(0.5f, 0.5f, 0f); 
-            
-            // Increase the cost for the next time
-            upgradeCost += 15; 
-            Debug.Log("Upgraded Vision! Next cost: " + upgradeCost);
+            if (upgradeCount >= MaxUpgrades)
+            {
+                // Final upgrade: reveal the entire screen by setting a very large scale
+                spotlightMask.localScale = new Vector3(100f, 100f, 1f);
+                Debug.Log("Max upgrades reached! Full screen revealed!");
+            }
+            else
+            {
+                // Grow the spotlight by the increment
+                spotlightMask.localScale = spotlightMask.localScale + new Vector3(inc_sl, inc_sl, 0f);
+
+                // Scale cost exponentially: next cost = current cost * scaleFactor (rounded to int)
+                upgradeCost = Mathf.RoundToInt(upgradeCost * costScaleFactor);
+                Debug.Log($"Upgraded Vision! Next cost: {upgradeCost} | Upgrades: {upgradeCount}/{MaxUpgrades}");
+            }
+
+            UpdateUI();
         }
         else
         {
